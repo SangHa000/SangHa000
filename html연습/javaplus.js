6798b3f7013a1ea812f343f483fdd1d6662bd79e
SangHa000 <144426104+SangHa000@users.noreply.github.com>
2024-03-08 09:02:36 +0900

    연습 정진!

---
+++ html연습/javaplus.js
@@ -0,0 +1,35 @@
+// 주석 요걸로 가능
+
+// 출력문 등의 명령문 그대로 작성
+// alert("hello world")
+
+// 출력문 4개
+alert("경고창");
+console.log("콘솔 상에서만 출력");
+
+document.write("html 페이지 상 출력");
+
+let st = document.getElementById("str");
+
+st.innerHTML="변경된 문구";
+
+// 자료형 (number, string, boolean)
+// 변수 및 상수 선언 (let, const)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
